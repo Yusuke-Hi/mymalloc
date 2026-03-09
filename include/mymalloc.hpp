1c51238a13ac233fc1c5be17538554c618f62414
yusuke <yuusuke29413@gmail.com>
2026-03-10 08:30:02 +0900

    ADD Obtaining large chunks of memory.

--- include/mymalloc.hpp
+++ include/mymalloc.hpp
@@ -3,6 +3,10 @@
 #ifndef MYMALLOC_HEADER
 #define MYMALLOC_HEADER
 
+extern bool first_call;
+extern const size_t kLargeChunkSize;
+extern void* LargeChunk;
+
 void* mymalloc(size_t size);
 
 #endif  // MYMALLOC_HEADER
--- src/mymalloc.cpp
+++ src/mymalloc.cpp
@@ -1,5 +1,21 @@
 #include "mymalloc.hpp"
 
+#include <errno.h>
+#include <stdio.h>
 #include <sys/mman.h>
 
-void* mymalloc(size_t size) {}
+bool first_call{true};
+const size_t kLargeChunkSize{1024 * 1024};
+void* LargeChunk{nullptr};
+
+void* mymalloc(size_t size) {
+  if (first_call) {
+    LargeChunk = mmap(NULL, kLargeChunkSize, PROT_READ | PROT_WRITE,
+                      MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (LargeChunk == MAP_FAILED) {
+      perror("mmap");
+      return nullptr;
+    }
+    first_call = false;
+  }
+}
