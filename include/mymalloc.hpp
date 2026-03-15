264a0d6c1ce69eade675952c3d51a20879fc7885
yusuke <yuusuke29413@gmail.com>
2026-03-15 12:07:19 +0900

    Create mymalloc as first version

--- include/mymalloc.hpp
+++ include/mymalloc.hpp
@@ -3,9 +3,11 @@
 #ifndef MYMALLOC_HEADER
 #define MYMALLOC_HEADER
 
+// TODO: parameters.hなどに宣言
 extern bool first_call;
 extern const size_t kLargeChunkSize;
 extern void* LargeChunk;
+extern MemoryHeader* free_list;
 
 void* mymalloc(size_t size);
 
--- src/mymalloc.cpp
+++ src/mymalloc.cpp
@@ -6,16 +6,44 @@
 
 bool first_call{true};
 const size_t kLargeChunkSize{1024 * 1024};
-void* LargeChunk{nullptr};
+MemoryHeader* free_list{nullptr};
 
-void* mymalloc(size_t size) {
+void* mymalloc(size_t arg_size) {
   if (first_call) {
-    LargeChunk = mmap(NULL, kLargeChunkSize, PROT_READ | PROT_WRITE,
-                      MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    if (LargeChunk == MAP_FAILED) {
+    free_list = static_cast<MemoryHeader*>(
+        mmap(NULL, kLargeChunkSize, PROT_READ | PROT_WRITE,
+             MAP_PRIVATE | MAP_ANONYMOUS, -1, 0));
+    if (free_list == MAP_FAILED) {
       perror("mmap");
       return nullptr;
     }
+
+    // initialize free_list
+    SetMemoryHeader(free_list, kLargeChunkSize - sizeof(MemoryHeader), true,
+                    nullptr);
+
     first_call = false;
   }
+
+  // search from the begining of free_list
+  MemoryHeader* current_header = free_list;
+  const size_t kCurrentBlockSize = sizeof(MemoryHeader) + arg_size;
+  while (current_header) {
+    if (current_header->is_free && current_header->size >= kCurrentBlockSize) {
+      // split current block
+      void* payload = static_cast<void*>(current_header + 1);
+      MemoryHeader* new_memory_header{reinterpret_cast<MemoryHeader*>(
+          static_cast<char*>(payload) + arg_size)};
+
+      SetMemoryHeader(new_memory_header,
+                      current_header->size - kCurrentBlockSize, true, nullptr);
+      // update current_header
+      SetMemoryHeader(current_header, arg_size, false, new_memory_header);
+
+      return payload;
+    }
+    current_header = current_header->next;
+  }
+
+  return nullptr;
 }
